d650c4dbff2cc0f33f0df6f80ff8ba5dbc943203
Dani Foldi <dani@stefan.hu>
2019-10-13 11:51:03 +0200

    Added default port overriding

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 const server = http.createServer(app)
 const socket = io(server)
 
+const port = process.env.PORT || 3000
+
 app.use(express.static('public'))
 
 socket.on('connection', socket => {
@@ -18,6 +20,6 @@ socket.on('connection', socket => {
   })
 })
 
-server.listen(3000, () => {
-  console.log('Listening on port 3000')
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
