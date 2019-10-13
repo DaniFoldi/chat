df4ce2aef73af81f23cd2b004af69d3ae0f9adfb
Dani Foldi <dani@stefan.hu>
2019-10-13 17:39:06 +0200

    Changed all functions to async

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-socket.on('connection', socket => {
+    })
+  })(req, res, next)
+})
+
+socket.on('connection', async socket => {
   socket.broadcast.emit('special', 'A user connected')
-  socket.on('disconnect', () => {
+  socket.on('disconnect', async () => {
     socket.broadcast.emit('special', 'A user disconnected')
   })
-  socket.on('message', data => {
+  socket.on('message', async data => {
     socket.broadcast.emit('message', data)
   })
 })
 
-server.listen(port, () => {
+server.listen(port, async () => {
   console.log(`Listening on port ${port}`)
 })
