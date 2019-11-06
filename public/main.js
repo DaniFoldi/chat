0ac8b9cf64a95b3e48f08f824182b479048c6107
Dani Foldi <dani@stefan.hu>
2019-11-06 22:31:31 +0100

    Added code highlighting

--- public/main.js
+++ public/main.js
@@ -7,6 +7,17 @@ const md = markdownit({
 md.use(markdownitSup)
 md.use(markdownitSub)
 md.use(markdownitEmoji)
+md.use(md => {
+  const temp = md.renderer.rules.fence.bind(md.renderer.rules)
+  md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+    const token = tokens[idx]
+    const code = token.content.trim()
+    if (token.info.length > 0) {
+      return `<pre><code class="hljs">${hljs.highlightAuto(code, [token.info]).value}</code></pre>`
+    }
+    return temp(tokens, idx, options, env, slf)
+  }
+})
 md.renderer.rules.emoji = (token, idx) => {
   return twemoji.parse(token[idx].content)
 }
