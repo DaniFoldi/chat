8a55bfaba048aff99ac700728cddcee9e8a9818b
Békei Dóra <lave.zey@gmail.com>
2019-11-09 16:03:22 +0100

    Added timestamps to messages

--- public/message.js
+++ public/message.js
@@ -3,6 +3,16 @@ class Message {
     this.properties = properties
     if (typeof this.properties.identifier === 'undefined')
       this.properties.identifier = uuid()
+    if (typeof this.properties.timestamp === 'undefined') {
+      let date = new Date()
+      let datea = date.toString().split(" ", 5)
+      let fdate = datea[0]
+      for (let i=1; i<datea.length; i++) {
+        fdate = fdate + " " + datea[i]
+      }
+      this.properties.timestamp = fdate
+    }
+
   }
   preprocess() {
     this.properties.displayed = this.properties.message
@@ -17,10 +27,14 @@ class Message {
     const container = document.createElement('div')
     container.classList.add('message')
     container.classList.add('message-' + this.properties.messagetype)
+    const timestamp = document.createElement('p')
+    timestamp.classList.add('timestamp')
+    timestamp.textContent = this.properties.timestamp
     if (emoji_regex.test(this.properties.displayed)) { // TODO: fix to work with all emojis
       container.classList.add('message-emoji')
     }
     container.innerHTML = this.properties.displayed
+    container.appendChild(timestamp)
     this.container = container
     return container
   }
