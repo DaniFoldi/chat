63403ff031282850b9f7d9e364e7f2627b1a46f7
Botond24 <44529287+Botond24@users.noreply.github.com>
2019-11-12 20:45:00 +0100

    Started the create chat function

--- public/conversationbar.js
+++ public/conversationbar.js
@@ -1,4 +1,4 @@
-var chatname = 'Main'
+let chatname = 'Main'
 const container = document.getElementById('sidebar')
 const button = document.createElement('button')
 container.appendChild(button)
@@ -17,3 +17,15 @@ button.addEventListener('click', () => {
   this.container.classList.add('message-replying')
   document.getElementById("input").focus()*/
 })
+function  createNewConverstaion(){
+  const containerCreate = document.getElementById('sidebar')
+  const buttonCreate = document.createElement('button')
+  container.appendChild(button)
+  buttonCreate.textContent =  'Create new chat'
+  buttonCreate.classList.add('chat-button')
+  buttonCreate.addEventListener('click', () => {
+    createChatStep1 = {
+        
+    }
+
+}
