7d9b354f0b251861933bd6f97a5462d8c1e157aa
Botond24 <44529287+Botond24@users.noreply.github.com>
2019-11-13 01:10:17 +0100

    Continued work on create Chat

--- public/conversationbar.js
+++ public/conversationbar.js
@@ -4,10 +4,11 @@ const button = document.createElement('button')
 container.appendChild(button)
 button.textContent = chatname + ' chat'
 button.classList.add('chat-button')
-
+createNewConverstaion()
 
 button.addEventListener('click', () => {
   replymessage = {
+
     user: 'anon',
     message: this.properties.message
   }
@@ -18,14 +19,25 @@ button.addEventListener('click', () => {
   document.getElementById("input").focus()*/
 })
 function  createNewConverstaion(){
+  let usernameList = []
   const containerCreate = document.getElementById('sidebar')
   const buttonCreate = document.createElement('button')
   container.appendChild(button)
   buttonCreate.textContent =  'Create new chat'
   buttonCreate.classList.add('chat-button')
   buttonCreate.addEventListener('click', () => {
-    createChatStep1 = {
-        
+    createChat = {
+      document.getElementById('createChatpopup').classList.add('shown')
+      document.getElementById('createChatpopup-content').innerHTML = `
+        <form>
+        <input type="text" id="username" placeholder="Usernames">
+        <button id="Create">Create chat</button>
+      </form>`
+      document.querySelector('#CreateChatpopup-content form').addEventListener('submit', async event => {
+        usernameList.push(document.getElementById('username').split(" , "))
+        console.log(usernameList);
+        getIdentifier()
+      })
     }
-
+  })
 }
