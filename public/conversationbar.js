b46b8509c5d1fe7fa1ec4244492dedcdcc8efdad
Botond24 <44529287+Botond24@users.noreply.github.com>
2019-11-16 17:27:35 +0100

    Update conversationbar.js

--- public/conversationbar.js
+++ public/conversationbar.js
@@ -7,13 +7,14 @@ function  createNewConversation(){
     document.getElementById('createChatpopup').classList.add('shown')
     document.getElementById('createChatpopup-content').innerHTML = `
     <form>
-      <input type="text" id="username" placeholder="Usernames">
+      <input type="text" id="usernames" placeholder="Usernames">
       <button id="Create">Create chat</button>
       <p id="members"></p>
     </form>`
     document.querySelector('#createChatpopup-content form').addEventListener('submit', async event => {
       event.preventDefault()
-      let usernameList = document.getElementById('username').innerHTML.split(", ")
+      let usernameList = document.getElementById('usernames').value.split(' ')
+      console.log(usernameList)
       document.getElementById('members').innerHTML = usernameList.join(", ")
     })
   })
